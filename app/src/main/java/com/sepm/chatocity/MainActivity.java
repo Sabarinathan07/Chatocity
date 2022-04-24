1288ccdfaaf3006c500771a54d1576fb729214b8
Sabari <r.sabarinathan22@gmail.com>
2022-04-24 23:00:22 +0530

    login and register added

--- app/src/main/java/com/sepm/chatocity/MainActivity.java
+++ app/src/main/java/com/sepm/chatocity/MainActivity.java
@@ -2,27 +2,43 @@ package com.sepm.chatocity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button loginButton,signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View view) {
-                                          Toast.makeText(MainActivity.this,"heloo", Toast.LENGTH_SHORT).show();
-                                      }
-                                  }
+        loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
 
+        loginButton.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+//                  Toast.makeText(MainActivity.this,"heloo", Toast.LENGTH_SHORT).show();
+                  Intent i = new Intent(MainActivity.this, Navigation.class);
+                  startActivity(i);
 
+              }
+          }
         );
+
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
 }
